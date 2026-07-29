921f5ea8fd7ea6774066f5fbb654b38e0cded5e4
pratikshakadam912 <pratiksha1177@gmail.com>
2026-07-29 19:14:50 +0530

    express app created

---
+++ backend/src/app.js
@@ -0,0 +1,32 @@
+import express from "express";
+import cors from "cors";
+import helmet from "helmet";
+import morgan from "morgan";
+import cookieParser from "cookie-parser";
+
+import authRoutes from "./routes/auth.routes.js";
+
+const app = express();
+
+app.use(
+  cors({
+    origin: "http://localhost:5173",
+    credentials: true,
+  }),
+);
+
+app.use(helmet());
+app.use(morgan("dev"));
+app.use(express.json());
+app.use(cookieParser());
+
+app.get("/", (req, res) => {
+  res.json({
+    success: true,
+    message: "🚀 PulseHR Backend Running Successfully",
+  });
+});
+
+app.use("/api/auth", authRoutes);
+
+export default app;
