20aa488c13d0f0fc2dca66f6d620a41939000ec0
pratikshakadam912 <pratiksha1177@gmail.com>
2026-08-01 12:23:19 +0530

    settings of employee account

---
+++ frontend/src/pages/employee/Settings.jsx
@@ -0,0 +1,9 @@
+import DashboardLayout from "../../layouts/DashboardLayout";
+
+export default function Settings() {
+  return (
+    <DashboardLayout>
+      <h1 className="text-3xl font-bold">Settings</h1>
+    </DashboardLayout>
+  );
+}
