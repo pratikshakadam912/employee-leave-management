1e9df909665bd9d15f221814ec9b7c7851887611
pratikshakadam912 <pratiksha1177@gmail.com>
2026-08-01 12:24:11 +0530

    routes added

--- frontend/src/routes/AppRoutes.jsx
+++ frontend/src/routes/AppRoutes.jsx
@@ -4,6 +4,11 @@ import Login from "../pages/auth/Login";
 import Register from "../pages/auth/Register";
 import NotFound from "../pages/NotFound";
 import Dashboard from "../pages/employee/Dashboard";
+import ApplyLeave from "../pages/employee/ApplyLeave";
+import LeaveHistory from "../pages/employee/LeaveHistory";
+import Notifications from "../pages/employee/Notifications";
+import Profile from "../pages/employee/Profile";
+import Settings from "../pages/employee/Settings";
 
 export default function AppRoutes() {
   return (
@@ -16,6 +21,17 @@ export default function AppRoutes() {
       <Route path="/register" element={<Register />} />
 
       <Route path="/employee/dashboard" element={<Dashboard />} />
+      <Route path="/employee/dashboard" element={<Dashboard />} />
+
+      <Route path="/employee/apply" element={<ApplyLeave />} />
+
+      <Route path="/employee/history" element={<LeaveHistory />} />
+
+      <Route path="/employee/notifications" element={<Notifications />} />
+
+      <Route path="/employee/profile" element={<Profile />} />
+
+      <Route path="/employee/settings" element={<Settings />} />
 
       {/* 404 */}
       <Route path="*" element={<NotFound />} />
