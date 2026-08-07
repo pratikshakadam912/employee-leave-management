c28944b2fc3d7d978d29c706943dbce95f2ec972
pratikshakadam912 <pratiksha1177@gmail.com>
2026-08-07 18:51:01 +0530

    CORS error

--- backend/src/app.js
+++ backend/src/app.js
@@ -13,15 +13,22 @@ const app = express();
 
 app.use(
   cors({
-    origin: [
-      process.env.CLIENT_URL,
-      "http://localhost:5173",
-      "http://localhost:5174",
-    ],
+    origin: (origin, callback) => {
+      const allowedOrigins = [
+        process.env.CLIENT_URL,
+        "http://localhost:5173",
+        "http://localhost:5174",
+      ];
+
+      if (!origin || allowedOrigins.includes(origin)) {
+        callback(null, true);
+      } else {
+        callback(new Error("Not allowed by CORS"));
+      }
+    },
     credentials: true,
   }),
 );
-
 app.use(helmet());
 app.use(morgan("dev"));
 app.use(express.json());
