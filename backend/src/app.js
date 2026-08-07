fa589e02508c3107b61937196581ae65f23d4441
pratikshakadam912 <pratiksha1177@gmail.com>
2026-08-07 12:12:11 +0530

    production ready

--- backend/src/app.js
+++ backend/src/app.js
@@ -14,7 +14,11 @@ const app = express();
 
 app.use(
   cors({
-    origin: ["http://localhost:5173", "http://localhost:5174"],
+    origin: [
+      process.env.CLIENT_URL,
+      "http://localhost:5173",
+      "http://localhost:5174",
+    ],
     credentials: true,
   }),
 );
