31b9aa9fe7cc8d71b99e71512284fc5ab284f3d9
pratikshakadam912 <pratiksha1177@gmail.com>
2026-08-08 13:51:49 +0530

    Fix production API URL

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,10 +1,24 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:5000/api",
+  baseURL: import.meta.env.VITE_API_URL,
   headers: {
     "Content-Type": "application/json",
   },
+  withCredentials: true,
 });
+
+api.interceptors.request.use(
+  (config) => {
+    const auth = JSON.parse(localStorage.getItem("auth"));
+
+    if (auth?.token) {
+      config.headers.Authorization = `Bearer ${auth.token}`;
+    }
+
+    return config;
+  },
+  (error) => Promise.reject(error),
+);
 
 export default api;
--- frontend/src/services/authService.js
+++ frontend/src/services/authService.js
@@ -1,4 +1,4 @@
-import api from "./api";
+import api from "../utils/axios";
 
 export const loginUser = async (data) => {
   const response = await api.post("/auth/login", data);
